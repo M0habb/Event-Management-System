91d3a711d20df6f519491c5acc9820d2446f3641
M0habb <mohab.abdullatif01@gmail.com>
2025-04-18 18:21:47 +0200

    Address Class Version 2
    Removed state attribute.
    Created constructor.
    Overrode toString() method.

--- src/Address.java
+++ src/Address.java
@@ -1,7 +1,17 @@
 public abstract class   Address {
     String city;
     String street;
-    String state;
     long postalCode;
     String country;
+    Address(String country, String city,String street, long postalCode){
+        this.country = country;
+        this.city = city;
+        this.street = street;
+        this.postalCode = postalCode;
+    }
+
+    @Override
+    public String toString() {
+        return street + ", " + city + ", " + country + ". " + postalCode;
+    }
 }
