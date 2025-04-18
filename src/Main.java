933664ed13db2f59dfcfdd11d63dc8fa8dc81897
M0habb <mohab.abdullatif01@gmail.com>
2025-04-18 21:05:15 +0200

    asda

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,7 @@ import java.util.Date;
 public class Main {
     public static void main(String[] args) {
         Database.initializeDummyData();
-<<<<<<< HEAD
         System.out.println("hi guys");
-=======
         System.out.println("Test"); //sadjajks
->>>>>>> b2edf9fabb0dee6431d5595b644a29a32cca242d
     }
 }
