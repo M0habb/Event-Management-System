a8d3ba891d066d4a3f06acaf0cd5636a88405ca8
SarahHaimy <saso.thomas2006@gmail.com>
2025-04-21 16:43:14 +0200

    updates

--- src/Address.java
+++ src/Address.java
@@ -1,8 +1,8 @@
 public class   Address {
-    String city;
-    String street;
-    long postalCode;
-    String country;
+    public String city;
+    public String street;
+    public long postalCode;
+    public String country;
     Address(String country, String city, String street, long postalCode){
         this.country = country;
         this.city = city;
