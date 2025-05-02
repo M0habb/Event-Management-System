d4265fa36a6fa28153268a1d961fc86d512359c3
M0habb <mohab.abdullatif01@gmail.com>
2025-05-03 01:09:32 +0300

    tg

--- src/main/java/classes/Controller.java
+++ src/main/java/classes/Controller.java
@@ -16,8 +16,8 @@ public class Controller extends Application{
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/resources/main.fxml"));
-        primaryStage.setTitle("JavaFX App");
-        primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.setTitle("Event Management System");
+        primaryStage.setScene(new Scene(root, 1142, 642));
         primaryStage.show();
     }
 }
