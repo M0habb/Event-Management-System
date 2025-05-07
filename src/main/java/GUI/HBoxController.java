9daf8e90d02d74923a2deba78088b273cee55697
SarahHaimy <saso.thomas2006@gmail.com>
2025-05-07 19:35:35 +0300

    z

---
+++ src/main/java/GUI/HBoxController.java
@@ -0,0 +1,16 @@
+package GUI;
+
+import javafx.fxml.FXML;
+import javafx.scene.*;
+import javafx.scene.control.Label;
+
+
+public class HBoxController {
+
+    @FXML
+    public void initiate(){
+        eventNameLabel=
+    }
+
+
+}
--- src/main/java/classes/Ticket.java
+++ src/main/java/classes/Ticket.java
@@ -1,4 +1,38 @@
 package classes;
-
+import java.util.Date;
 public class Ticket {
+    private Attendee owner;
+    private double fees;
+    private Date date;
+    Ticket(Attendee owner, double fees, Date date){
+        this.owner=owner;
+        this.fees=fees;
+        this.date=date;
+
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Attendee getOwner() {
+        return owner;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public double getFees() {
+        return fees;
+    }
+
+    public void setFees(double fees) {
+        this.fees = fees;
+    }
+
+    public void setOwner(Attendee owner) {
+        this.owner = owner;
+    }
+
 }
