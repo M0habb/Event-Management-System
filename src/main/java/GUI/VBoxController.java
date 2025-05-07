9f77df141a48294ff8cc2682ff04fc590c1c1585
M0habb <mohab.abdullatif01@gmail.com>
2025-05-07 20:46:12 +0300

    d

--- src/main/java/GUI/VBoxController.java
+++ src/main/java/GUI/VBoxController.java
@@ -2,6 +2,7 @@ package GUI;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 
@@ -9,8 +10,9 @@ public class VBoxController{
 
     @FXML private Label eventName;
     @FXML private Label eventDate1;
+    @FXML private Button buyButton;
 
-
+    boolean ticketPurchased = false;
 
     @FXML private VBox expandableBox;
 
@@ -27,6 +29,12 @@ public class VBoxController{
 
     @FXML
     public void handleBuy(ActionEvent event){
-        ViewEventsController.handleBuy(event, eventName);
+
+        if (!ticketPurchased){
+            ticketPurchased = true;
+            buyButton.setText("Ticket Added to Cart!");
+        }else return;
+
+        ViewEventsController.handleBuy(eventName);
     }
 }
