c470a6d59b31746a2f8ad25a89c1e52d4d35ff75
M0habb <mohab.abdullatif01@gmail.com>
2025-05-07 03:48:31 +0300

    s

--- src/main/java/GUI/ViewEventsController.java
+++ src/main/java/GUI/ViewEventsController.java
@@ -1,14 +1,13 @@
 package GUI;
 
-import classes.Attendee;
-import classes.Database;
-import classes.User;
+import classes.*;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.chart.PieChart;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
@@ -41,6 +40,23 @@ public class ViewEventsController {
     @FXML
     public void initialize() {
 
+        displayEvents();
+
+        usernameLabel.setText(currentUser.getUserName());
+
+        categoryButtons = Arrays.asList(musicCategory, sportsCategory, theaterCategory, conferenceCategory, otherCategory);
+
+        // Assign each button its unique color
+        categoryButtonColors = new HashMap<>();
+        categoryButtonColors.put(musicCategory, "#f1c40f");
+        categoryButtonColors.put(sportsCategory, "#16a085");
+        categoryButtonColors.put(theaterCategory, "#3498db");
+        categoryButtonColors.put(conferenceCategory, "#9b59b6");
+        categoryButtonColors.put(otherCategory, "#95a5a6");
+    }
+
+    private void displayEvents(){
+        VBox rootV = (VBox) scrollpane.getContent();
         for (int i =0; i < Database.events.size(); i++){
             VBox newVBox;
             try {
@@ -66,58 +82,305 @@ public class ViewEventsController {
             Label eventActivities = (Label) innerV.getChildren().get(3);
             Label eventFees = (Label) innerV.getChildren().get(4);
 
-            VBox rootV = (VBox) scrollpane.getContent();
+            Address address = Database.events.get(i).getAddress();
+
+            eventLoc.setText(eventLoc.getText() + " " + address.country + ", " + address.city + ", " + address.street + ".");
+            eventOrganizer.setText(eventOrganizer.getText() + " " +  Database.events.get(i).getOrganizer().getUserName() + ".");
+            String outdoors = "outdoors.";
+            if (!Database.events.get(i).getOutdoors()) outdoors = "indoors.";
+            eventOutdoors.setText(eventOutdoors.getText() +  outdoors);
+            eventFees.setText(eventFees.getText() + " " +  Database.events.get(i).getFees() + ".");
+
             rootV.getChildren().add(newVBox);
         }
-
-        usernameLabel.setText(currentUser.getUserName());
-
-        categoryButtons = Arrays.asList(musicCategory, sportsCategory, theaterCategory, conferenceCategory, otherCategory);
-
-        // Assign each button its unique color
-        categoryButtonColors = new HashMap<>();
-        categoryButtonColors.put(musicCategory, "#f1c40f");
-        categoryButtonColors.put(sportsCategory, "#16a085");
-        categoryButtonColors.put(theaterCategory, "#3498db");
-        categoryButtonColors.put(conferenceCategory, "#9b59b6");
-        categoryButtonColors.put(otherCategory, "#95a5a6");
     }
-
-    private void toggleBackground(Button clickedButton) {
+    private boolean toggleBackground(Button clickedButton) {
+        boolean colored = true;
+        String color = categoryButtonColors.get(clickedButton);
+        if (!clickedButton.getStyle().contains(color)){
+            colored = false;
+        }
         for (Button b : categoryButtons) {
             b.setStyle("-fx-background-color: transparent;");
         }
 
         // Apply the unique color if it's not already set
-        String color = categoryButtonColors.get(clickedButton);
-        if (!clickedButton.getStyle().contains(color)) {
+
+        if (!colored) {
             clickedButton.setStyle("-fx-background-color: " + color + ";");
+            return true;
         }
+        return false;
     }
 
     @FXML
     private void handleMusicCategory(){
-        toggleBackground(musicCategory);
+        VBox rootV = (VBox) scrollpane.getContent();
+        boolean toggle = toggleBackground(musicCategory);
+
+        if (toggle){
+
+            rootV.getChildren().clear();
+
+            for (int i =0; i < Database.events.size(); i++){
+
+                if(Database.events.get(i).getCategory().getType().equals(CategoryType.MUSIC)){
+                    VBox newVBox;
+                    try {
+                        newVBox = FXMLLoader.load(getClass().getResource("/resources/vbox_layout.fxml"));
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                    Node hbox1 = newVBox.getChildren().get(0);
+                    HBox hbox = (HBox) hbox1;
+                    Label eventName = (Label) hbox.getChildren().get(0);
+                    Label eventDate = (Label) hbox.getChildren().get(1);
+
+                    eventName.setText(Database.events.get(i).getEventName());
+                    Date date = Database.events.get(i).getEventDate();
+                    SimpleDateFormat formatter = new SimpleDateFormat("EEE, MMM dd, yyyy");
+                    String dateString = formatter.format(date);
+                    eventDate.setText(dateString);
+
+                    VBox innerV = (VBox) newVBox.getChildren().get(1);
+                    Label eventLoc = (Label) innerV.getChildren().get(0);
+                    Label eventOrganizer = (Label) innerV.getChildren().get(1);
+                    Label eventOutdoors = (Label) innerV.getChildren().get(2);
+                    Label eventActivities = (Label) innerV.getChildren().get(3);
+                    Label eventFees = (Label) innerV.getChildren().get(4);
+
+                    Address address = Database.events.get(i).getAddress();
+
+                    eventLoc.setText(eventLoc.getText() + " " + address.country + ", " + address.city + ", " + address.street + ".");
+                    eventOrganizer.setText(eventOrganizer.getText() + " " +  Database.events.get(i).getOrganizer().getUserName() + ".");
+                    String outdoors = "outdoors.";
+                    if (!Database.events.get(i).getOutdoors()) outdoors = "indoors.";
+                    eventOutdoors.setText(eventOutdoors.getText() +  outdoors);
+                    eventFees.setText(eventFees.getText() + " " +  Database.events.get(i).getFees() + ".");
+
+                    rootV.getChildren().add(newVBox);
+                }
+            }
+        }else {
+            System.out.println("I am working");
+            displayEvents();
+        }
     }
 
     @FXML
     private void handleSportsCategory(){
-        toggleBackground(sportsCategory);
+        VBox rootV = (VBox) scrollpane.getContent();
+        boolean toggle = toggleBackground(sportsCategory);
+
+        if (toggle){
+
+            rootV.getChildren().clear();
+
+            for (int i =0; i < Database.events.size(); i++){
+
+                if(Database.events.get(i).getCategory().getType().equals(CategoryType.SPORTS)){
+                    VBox newVBox;
+                    try {
+                        newVBox = FXMLLoader.load(getClass().getResource("/resources/vbox_layout.fxml"));
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                    Node hbox1 = newVBox.getChildren().get(0);
+                    HBox hbox = (HBox) hbox1;
+                    Label eventName = (Label) hbox.getChildren().get(0);
+                    Label eventDate = (Label) hbox.getChildren().get(1);
+
+                    eventName.setText(Database.events.get(i).getEventName());
+                    Date date = Database.events.get(i).getEventDate();
+                    SimpleDateFormat formatter = new SimpleDateFormat("EEE, MMM dd, yyyy");
+                    String dateString = formatter.format(date);
+                    eventDate.setText(dateString);
+
+                    VBox innerV = (VBox) newVBox.getChildren().get(1);
+                    Label eventLoc = (Label) innerV.getChildren().get(0);
+                    Label eventOrganizer = (Label) innerV.getChildren().get(1);
+                    Label eventOutdoors = (Label) innerV.getChildren().get(2);
+                    Label eventActivities = (Label) innerV.getChildren().get(3);
+                    Label eventFees = (Label) innerV.getChildren().get(4);
+
+                    Address address = Database.events.get(i).getAddress();
+
+                    eventLoc.setText(eventLoc.getText() + " " + address.country + ", " + address.city + ", " + address.street + ".");
+                    eventOrganizer.setText(eventOrganizer.getText() + " " +  Database.events.get(i).getOrganizer().getUserName() + ".");
+                    String outdoors = "outdoors.";
+                    if (!Database.events.get(i).getOutdoors()) outdoors = "indoors.";
+                    eventOutdoors.setText(eventOutdoors.getText() +  outdoors);
+                    eventFees.setText(eventFees.getText() + " " +  Database.events.get(i).getFees() + ".");
+
+                    rootV.getChildren().add(newVBox);
+                }
+            }
+        }else {
+            System.out.println("I am working");
+            displayEvents();
+        }
     }
 
     @FXML
     private void handleTheaterCategory(){
-        toggleBackground(theaterCategory);
+        VBox rootV = (VBox) scrollpane.getContent();
+        boolean toggle = toggleBackground(theaterCategory);
+
+        if (toggle){
+
+            rootV.getChildren().clear();
+
+            for (int i =0; i < Database.events.size(); i++){
+
+                if(Database.events.get(i).getCategory().getType().equals(CategoryType.THEATER)){
+                    VBox newVBox;
+                    try {
+                        newVBox = FXMLLoader.load(getClass().getResource("/resources/vbox_layout.fxml"));
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                    Node hbox1 = newVBox.getChildren().get(0);
+                    HBox hbox = (HBox) hbox1;
+                    Label eventName = (Label) hbox.getChildren().get(0);
+                    Label eventDate = (Label) hbox.getChildren().get(1);
+
+                    eventName.setText(Database.events.get(i).getEventName());
+                    Date date = Database.events.get(i).getEventDate();
+                    SimpleDateFormat formatter = new SimpleDateFormat("EEE, MMM dd, yyyy");
+                    String dateString = formatter.format(date);
+                    eventDate.setText(dateString);
+
+                    VBox innerV = (VBox) newVBox.getChildren().get(1);
+                    Label eventLoc = (Label) innerV.getChildren().get(0);
+                    Label eventOrganizer = (Label) innerV.getChildren().get(1);
+                    Label eventOutdoors = (Label) innerV.getChildren().get(2);
+                    Label eventActivities = (Label) innerV.getChildren().get(3);
+                    Label eventFees = (Label) innerV.getChildren().get(4);
+
+                    Address address = Database.events.get(i).getAddress();
+
+                    eventLoc.setText(eventLoc.getText() + " " + address.country + ", " + address.city + ", " + address.street + ".");
+                    eventOrganizer.setText(eventOrganizer.getText() + " " +  Database.events.get(i).getOrganizer().getUserName() + ".");
+                    String outdoors = "outdoors.";
+                    if (!Database.events.get(i).getOutdoors()) outdoors = "indoors.";
+                    eventOutdoors.setText(eventOutdoors.getText() +  outdoors);
+                    eventFees.setText(eventFees.getText() + " " +  Database.events.get(i).getFees() + ".");
+
+                    rootV.getChildren().add(newVBox);
+                }
+            }
+        }else {
+            System.out.println("I am working");
+            displayEvents();
+        }
     }
 
     @FXML
     private void handleConferenceCategory(){
-        toggleBackground(conferenceCategory);
+        VBox rootV = (VBox) scrollpane.getContent();
+        boolean toggle = toggleBackground(conferenceCategory);
+
+        if (toggle){
+
+            rootV.getChildren().clear();
+
+            for (int i =0; i < Database.events.size(); i++){
+
+                if(Database.events.get(i).getCategory().getType().equals(CategoryType.CONFERENCE)){
+                    VBox newVBox;
+                    try {
+                        newVBox = FXMLLoader.load(getClass().getResource("/resources/vbox_layout.fxml"));
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                    Node hbox1 = newVBox.getChildren().get(0);
+                    HBox hbox = (HBox) hbox1;
+                    Label eventName = (Label) hbox.getChildren().get(0);
+                    Label eventDate = (Label) hbox.getChildren().get(1);
+
+                    eventName.setText(Database.events.get(i).getEventName());
+                    Date date = Database.events.get(i).getEventDate();
+                    SimpleDateFormat formatter = new SimpleDateFormat("EEE, MMM dd, yyyy");
+                    String dateString = formatter.format(date);
+                    eventDate.setText(dateString);
+
+                    VBox innerV = (VBox) newVBox.getChildren().get(1);
+                    Label eventLoc = (Label) innerV.getChildren().get(0);
+                    Label eventOrganizer = (Label) innerV.getChildren().get(1);
+                    Label eventOutdoors = (Label) innerV.getChildren().get(2);
+                    Label eventActivities = (Label) innerV.getChildren().get(3);
+                    Label eventFees = (Label) innerV.getChildren().get(4);
+
+                    Address address = Database.events.get(i).getAddress();
+
+                    eventLoc.setText(eventLoc.getText() + " " + address.country + ", " + address.city + ", " + address.street + ".");
+                    eventOrganizer.setText(eventOrganizer.getText() + " " +  Database.events.get(i).getOrganizer().getUserName() + ".");
+                    String outdoors = "outdoors.";
+                    if (!Database.events.get(i).getOutdoors()) outdoors = "indoors.";
+                    eventOutdoors.setText(eventOutdoors.getText() +  outdoors);
+                    eventFees.setText(eventFees.getText() + " " +  Database.events.get(i).getFees() + ".");
+
+                    rootV.getChildren().add(newVBox);
+                }
+            }
+        }else {
+            System.out.println("I am working");
+            displayEvents();
+        }
     }
 
     @FXML
     private void handleOtherCategory(){
-        toggleBackground(otherCategory);
+        VBox rootV = (VBox) scrollpane.getContent();
+        boolean toggle = toggleBackground(otherCategory);
+
+        if (toggle){
+
+            rootV.getChildren().clear();
+
+            for (int i =0; i < Database.events.size(); i++){
+
+                if(Database.events.get(i).getCategory().getType().equals(CategoryType.OTHER)){
+                    VBox newVBox;
+                    try {
+                        newVBox = FXMLLoader.load(getClass().getResource("/resources/vbox_layout.fxml"));
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                    Node hbox1 = newVBox.getChildren().get(0);
+                    HBox hbox = (HBox) hbox1;
+                    Label eventName = (Label) hbox.getChildren().get(0);
+                    Label eventDate = (Label) hbox.getChildren().get(1);
+
+                    eventName.setText(Database.events.get(i).getEventName());
+                    Date date = Database.events.get(i).getEventDate();
+                    SimpleDateFormat formatter = new SimpleDateFormat("EEE, MMM dd, yyyy");
+                    String dateString = formatter.format(date);
+                    eventDate.setText(dateString);
+
+                    VBox innerV = (VBox) newVBox.getChildren().get(1);
+                    Label eventLoc = (Label) innerV.getChildren().get(0);
+                    Label eventOrganizer = (Label) innerV.getChildren().get(1);
+                    Label eventOutdoors = (Label) innerV.getChildren().get(2);
+                    Label eventActivities = (Label) innerV.getChildren().get(3);
+                    Label eventFees = (Label) innerV.getChildren().get(4);
+
+                    Address address = Database.events.get(i).getAddress();
+
+                    eventLoc.setText(eventLoc.getText() + " " + address.country + ", " + address.city + ", " + address.street + ".");
+                    eventOrganizer.setText(eventOrganizer.getText() + " " +  Database.events.get(i).getOrganizer().getUserName() + ".");
+                    String outdoors = "outdoors.";
+                    if (!Database.events.get(i).getOutdoors()) outdoors = "indoors.";
+                    eventOutdoors.setText(eventOutdoors.getText() +  outdoors);
+                    eventFees.setText(eventFees.getText() + " " +  Database.events.get(i).getFees() + ".");
+
+                    rootV.getChildren().add(newVBox);
+                }
+            }
+        }else {
+            System.out.println("I am working");
+            displayEvents();
+        }
     }
 
     @FXML
