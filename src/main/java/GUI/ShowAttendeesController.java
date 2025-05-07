9beef46291b048720cab761411493a0cb4101b9b
mohamed-sameh777 <modisameh@gmail.com>
2025-05-07 03:52:41 +0300

    .

--- src/main/java/GUI/ShowAttendeesController.java
+++ src/main/java/GUI/ShowAttendeesController.java
@@ -1,4 +1,34 @@
 package GUI;
 
+import classes.Admin;
+import classes.Attendee;
+import classes.Database;
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.beans.property.SimpleStringProperty;
+
 public class ShowAttendeesController {
+
+    @FXML private TableView<Attendee> attendeeTable;
+    @FXML private TableColumn<Attendee, String> usernameColumn;
+    @FXML private TableColumn<Attendee, String> balanceColumn;
+
+    @FXML
+    public void initialize() {
+        // Column binding
+        usernameColumn.setCellValueFactory(cellData ->
+                new SimpleStringProperty(cellData.getValue().getUserName())
+        );
+
+        balanceColumn.setCellValueFactory(cellData ->
+                new SimpleStringProperty(
+                        String.valueOf(cellData.getValue().getWallet().getBalance())
+                )
+        );
+
+        // Fill table with all attendees from the database
+        attendeeTable.setItems(FXCollections.observableArrayList(Database.totalAttendees));
+    }
 }
