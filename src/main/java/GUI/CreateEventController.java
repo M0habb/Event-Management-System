0ae5e153eed526f4afc5147d117c227030384065
jana-hisham <janahishamw@gmail.com>
2025-05-08 17:18:18 +0300

    q

--- src/main/java/GUI/CreateEventController.java
+++ src/main/java/GUI/CreateEventController.java
@@ -1,14 +1,24 @@
 package GUI;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
-import java.awt.*;
+import java.io.IOException;
+
 
 public class CreateEventController {
+
     @FXML
     private TextField typeTextfield;
-
 
     @FXML
     private TextField nameTextfield;
@@ -20,12 +30,38 @@ public class CreateEventController {
     private TextField priceTextField;
 
     @FXML
-    private TextField dateTextField;
+    private DatePicker eventDate;
 
     @FXML
-    private Checkbox outdoorsCheckbox;
+    private CheckBox outdoorsCheckbox;
 
-    
+    @FXML
+    private void handleBack(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/resources/organizerLanding.fxml"));
 
+        Scene scene = new Scene(root, 1142, 642);
+
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+        window.setScene(scene);
+        window.show();
+    }
+    @FXML
+    private void handleCreate (ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/resources/organizerLanding.fxml"));
+
+        Scene scene = new Scene(root, 1142, 642);
+
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+        window.setScene(scene);
+        window.show();
+    }
+
+    @FXML
+    private void handleButtonClick() {
+        
+
+    }
 }
 
