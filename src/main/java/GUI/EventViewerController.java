8ab2a63b40051c2ecb19008471c450da3db1c1cc
M0habb <mohab.abdullatif01@gmail.com>
2025-05-10 04:48:43 +0300

    s

--- src/main/java/GUI/EventViewerController.java
+++ src/main/java/GUI/EventViewerController.java
@@ -6,12 +6,14 @@ import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
@@ -71,6 +73,9 @@ public class EventViewerController {
 
     @FXML
     private void initialize(){
+
+        StackPane.setAlignment(nameText, Pos.TOP_LEFT);
+        StackPane.setAlignment(nameTextField, Pos.TOP_LEFT);
 
         rooms.setVisible(false);
         priceTextField.setVisible(false);
